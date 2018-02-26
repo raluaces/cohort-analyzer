6473e7d1dc817f7652dbbc5f08d4998dc373892d
raluaces <ralph@luac.es>
2018-02-25 20:44:08 -0800

    add support for html table output, code cleanup

--- analyze.py
+++ analyze.py
@@ -23,12 +23,18 @@ parser.add_argument(
 parser.add_argument(
     '--cohorts',
     help='number of corhorts to analyze (default 8)',
+    type=int,
     default=8
 )
 parser.add_argument(
     '--bucket-length',
     help='bucket length in days(default 6 days)',
+    type=int,
     default=6
+)
+parser.add_argument(
+    '--html-output',
+    help='html file to output results table to)'
 )
 
 args = parser.parse_args()
@@ -37,6 +43,8 @@ order_csv_file = args.orders
 cohort_length = timedelta(days=args.cohort_length)
 number_of_cohorts = args.cohorts
 bucket_length = timedelta(days=args.bucket_length)
+html_output_file = args.html_output
+
 
 # set our date format for parsing dates
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -76,9 +84,16 @@ with open(order_csv_file, 'r') as csvfile:
         }
 
 
+if html_output_file:
+    html_file = open(html_output_file, 'w')
+
+
 # our table printer for giving the user tables
 def table_printer(array_of_row_arrays, column_title_list, column_length=20):
+    # unify spaces for stdout table
     def unify_spaces(value):
+        if html_output_file:
+            return value
         string = str(value)
         chars = len(string)
         if chars < column_length:
@@ -86,44 +101,64 @@ def table_printer(array_of_row_arrays, column_title_list, column_length=20):
             string += ' ' * spaces
         return string
 
+    # function for logic of printing rows
     def row_printer(row_string, second_row=False):
+        if html_output_file:
+            html_file.write('<tr>{}<tr>'.format(row_string))
+            return
         print(row_string)
         if second_row:
             print(second_row)
         print(table_row_line)
 
-    #get our number of columns
-    num_of_columns = len(column_title_list)
-    table_row_line = '|'
-    # build a line and print the line
-    for i in range(num_of_columns):
-        table_row_line += '-' * column_length
-        table_row_line += '|'
-    print(table_row_line)
+    if html_output_file:
+        html_file.write('<html><table>')
+        cells = ''
+        for title in column_title_array:
+            cells += '<th>{}</th>'.format(title)
+        row_printer(cells)
+        for row in array_of_row_arrays:
+            if html_output_file:
+                html_row = '<tr>{}</tr>'
+            cells = ''
+            for cell in row:
+                cells += '<td>{}</td>'.format(cell)
+            row_printer(html_row.format(cells))
+        html_file.write('</table></html>')
+        html_file.close()
+        return
+    else:
+        # handle logic for printing a table to cli if wanted
+        # get our number of columns
+        num_of_columns = len(column_title_list)
+        table_row_line = '|'
+        # build a line and print the line
+        for i in range(num_of_columns):
+            table_row_line += '-' * column_length
+            table_row_line += '|'
+        print(table_row_line)
 
-    first_row = '|'
-    for title in column_title_list:
-        first_row += unify_spaces(title) + '|'
-    row_printer(first_row)
-
-    for row in array_of_row_arrays:
-        second_row_bool = False
-        printable_row = '|'
-        second_row = '|'
-        for cell in row:
-            if ',' in cell:
-                second_row_bool = True
-                cell = cell.split(',')
-                second_row += unify_spaces(cell[1]) + '|'
-                printable_row += unify_spaces(cell[0]) + '|'
+        first_row = '|'
+        for title in column_title_list:
+            first_row += unify_spaces(title) + '|'
+        row_printer(first_row)
+        for row in array_of_row_arrays:
+            second_row_bool = False
+            printable_row = '|'
+            second_row = '|'
+            for cell in row:
+                if ',' in cell:
+                    second_row_bool = True
+                    cell = cell.split(',')
+                    second_row += unify_spaces(cell[1]) + '|'
+                    printable_row += unify_spaces(cell[0]) + '|'
+                else:
+                    printable_row += unify_spaces(cell) + '|'
+                    second_row += unify_spaces(' ') + '|'
+            if second_row_bool:
+                row_printer(printable_row, second_row)
             else:
-                printable_row += unify_spaces(cell) + '|'
-                second_row += unify_spaces(' ') + '|'
-        if second_row_bool:
-            row_printer(printable_row, second_row)
-        else:
-            row_printer(printable_row)
-
+                row_printer(printable_row)
 
 
 
@@ -135,20 +170,31 @@ first_order = orders[min(orders, key=lambda x: orders[x]['created'])]['created']
 last_order = orders[max(orders, key=lambda x: orders[x]['created'])]['created']
 
 # get the start and end dates for our data for laters
-first_customer = customers[min(customers, key=lambda x: customers[x]['created'])]['created'].replace(hour=0, minute=0, second=0, microsecond=0)
-last_customer = customers[max(customers, key=lambda x: customers[x]['created'])]['created']
+first_customer = customers[
+    min(
+        customers,
+        key=lambda x: customers[x]['created']
+    )
+]['created'].replace(hour=0, minute=0, second=0, microsecond=0)
+last_customer = customers[
+    max(
+        customers,
+        key=lambda x: customers[x]['created']
+    )
+]['created']
 
-# get the earliest order
-# get the elapsed time of the data we have
-
+# establish counter for customers, will use later for calculations
 customer_count = 0
 
+# establish how many cohorts we have to work with in the data sample
 sample_length = (last_customer - first_customer).days
 number_of_cohorts_in_data = sample_length / cohort_length.days
 print('Analyzing {} customers with {} orders'.format(total_orders, total_customers))
 print('There are {}  {} day cohorts in this data'.format(number_of_cohorts_in_data, cohort_length.days))
+# let the user know how many cohorts from data start we want to analyse
 print('We are analyzing {} cohorts.'.format(number_of_cohorts))
 
+# start are array of column titles
 column_title_array = ['Cohort', 'Customers']
 
 table_rows = []
@@ -202,14 +248,16 @@ for cohort_count in range(number_of_cohorts):
                             first_timers += 1
         percent_orderers = int(orderers * (100 / len(cohort_customer_ids)))
         percent_first_time = int(first_timers * (100 / len(cohort_customer_ids)))
-        row.append('{}% orderers ({}),{}% 1st time ({})'.format(
+        if html_output_file:
+            delim = '<br/>'
+        else:
+            delim = ','
+        row.append('{}% orderers ({}){}{}% 1st time ({})'.format(
             percent_orderers,
             orderers,
+            delim,
             percent_first_time,
             first_timers
         ))
     table_rows.append(row)
-print('')
 table_printer(table_rows, column_title_array)
-
-
